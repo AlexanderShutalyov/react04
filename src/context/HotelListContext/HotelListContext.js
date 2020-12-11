00dbc2bfcd75724d963623ac7e5d4c63cd10a21d
Alexander <alexander@MacBook-Pro-Valery.local>
2020-12-11 23:52:10 +0200

    base css added

--- src/context/HotelListContext/HotelListContext.js
+++ src/context/HotelListContext/HotelListContext.js
@@ -9,8 +9,8 @@ export const HotelListContextProvider = ({ children }) => {
         fetch(`https://hotels4.p.rapidapi.com/properties/list?${new URLSearchParams({
             "destinationId": "1506246",
             "pageNumber": "1",
-            "checkIn": "2020-12-10",
-            "checkOut": "2020-12-25",
+            "checkIn": "2020-12-20",
+            "checkOut": "2020-12-30",
             "pageSize": "25",
             "adults1": "1",
             "currency": "USD",
@@ -19,22 +19,21 @@ export const HotelListContextProvider = ({ children }) => {
         })}`, {
             method: 'GET',
             headers: {
-                "x-rapidapi-key": "ef6aa709c6msh59ea8aef79713cbp1df7c5jsn167c2866f9b9",
+                "x-rapidapi-key": "2f45f06f8cmshe1835f919d4c781p128bbejsn99d34f98151d",
                 "x-rapidapi-host": "hotels4.p.rapidapi.com",
                 "useQueryString": true
             }
         })
-
             .then(data => data.json())
             .then(json => setHotelsData(json.data.body.searchResults.results))
-            // .then(json => console.log(json))
             .catch((error) => console.log(error))
-    }, [])
+    }, []);
     return (
-//        {/*<HotelListContext.Provider value={{ hotels: hotels, setHotelsData: setHotelsData }}>*/}
         <HotelListContext.Provider value={{ hotels: hotels, setHotelsData: setHotelsData }}>
             { children }
             {/*{ useMemo(() => (children), [users]) }*/}
         </HotelListContext.Provider>
     )
-}
+};
+// https://rapidapi.com/apidojo/api/hotels4/pricing
+// "x-rapidapi-key": "ef6aa709c6msh59ea8aef79713cbp1df7c5jsn167c2866f9b9",
