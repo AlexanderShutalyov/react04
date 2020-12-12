d6c69c7d352cb7fed6013ac615bb1297667539a5
Alexander <alexander@MacBook-Pro-Valery.local>
2020-12-12 12:39:31 +0200

    added default coordinates

--- src/components/MyMapComponent/MyMapComponent.js
+++ src/components/MyMapComponent/MyMapComponent.js
@@ -4,10 +4,11 @@ import {HotelListContext} from "../../context/HotelListContext/HotelListContext"
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) => {
     const {hotels, setHotelsData} = useContext(HotelListContext);
+    const defaultCenter = hotels.length > 0 ? {lat: hotels[0].coordinate.lat, lng: hotels[0].coordinate.lon} : {lat: 40.669612, lng: -73.811325};
 
     return <GoogleMap
             defaultZoom={8}
-            defaultCenter={{lat: 40.669612, lng: -73.811325}}
+            defaultCenter={ defaultCenter }
         >
             {props.isMarkerShown && hotels.map((props, index) => (<Marker key={index} position={{ lat: props.coordinate.lat, lng: props.coordinate.lon }} />))}
         </GoogleMap>
